52e265b5a62123ea0cfef780d4085814579b4574
Orbiba Robotics DevOps Teams <devop@orbibarobotics.com>
2023-01-12 22:00:52 +0300

    second commit

--- clone-dictionary/src/components/Header/Header.js
+++ clone-dictionary/src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Logo from '../Logo/Logo'
+import Search from '../Search/Search'
 import User from '../User/User'
 import { Styled } from './Header.styled'
 
 const Header = () => {
   return (
-    <Styled>
-      Header
+    <Styled className='header'>
       <Logo />
+      <Search />
       <User />
     </Styled>
   )
--- clone-dictionary/src/components/Header/Header.styled.js
+++ clone-dictionary/src/components/Header/Header.styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
 export const Styled = styled.div`
-
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
 `
